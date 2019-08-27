cf805963fa34d8e369cd95c8289a14e9b152a974
alen9968 <xwzj0608@qq.com>
2019-08-27 10:28:45 +0800

    bin/build.sh build

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker run  -v $(pwd):/usr/src/app/ -it --rm rails-pangu bundle install
 docker build -t rails-pangu -f Dockerfile .
+docker tag rails-pangu alen9968/rails-pangu
+if [ "$1" = "push" ]; then
+  docker push alen9968/rails-pangu
+fi
